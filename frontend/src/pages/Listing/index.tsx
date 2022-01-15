166ebb3c9b9b42dfa3c1775a8dc44ffb0abf8038
Rafael Nordio <rafael.nordio@gmail.com>
2022-01-15 19:31:12 -0300

    First Request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 
 import Pagination from "components/Pagination";
 import { Fragment } from "react";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+
+    //TESTE
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response => {
+            console.log(response.data);
+        });
+
 
     return (
         <>
